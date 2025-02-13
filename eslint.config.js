839362939024b25523c8b13ed75450ddfd220bd0
tris <nexus4fortune@gmail.com>
2025-02-14 07:30:30 +0900

    chore: eslint-plugin-react-hooksを導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  pluginJs.configs.recommended,
+  {
+    ignores: ["dist", "src-tauri"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { "react-hooks": reactHooks },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
